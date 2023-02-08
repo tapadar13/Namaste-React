cfa4e1a2aca641ee2ae28749644c471dd19fbd07
tapadar13 <monsurismyname@gmail.com>
2023-02-08 12:17:01 +0530

    Created nested React elements using createElement

--- App.js
+++ App.js
@@ -3,13 +3,33 @@
 // Creating nested React elements
 const heading = React.createElement("h1", {
     id: "title-1",
+    key: "first",
     style: {color: "#626fe6"},
 }, "Hello, React!");
 
 console.log(heading);
 
-// Creating the root using the createRoot API
+const heading2 = React.createElement("h1", {
+    id: "title-2",
+    key: "second",
+    style: {color: "#e8630a"},
+}, "Bonjour, React!");
+
+const heading3 = React.createElement("h1", {
+    id: "title-3",
+    key: "third",
+    style: {color: "f8b400"},
+}, "Ciao, React!");
+
+const container = React.createElement("div", {
+    id: "container",
+    key: "container-one",
+}, [heading, heading2, heading3]);
+
+console.log(container);
+
+// Creating the root using createRoot
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// Rendering the element
-root.render(heading);
+// Rendering the elements
+root.render(container);
