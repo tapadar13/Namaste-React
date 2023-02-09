8a01b274092033c9fead595625a6fc63a8fcc697
tapadar13 <monsurismyname@gmail.com>
2023-02-09 20:56:05 +0530

    added some style

--- App.js
+++ App.js
@@ -1,38 +1,81 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Manipulating DOM using React
+/**
+ * Header
+ *  - Logo/Title
+ *  - Nav Items
+ *  - Cart
+ * Body
+ *  - Search Bar
+ *  - Restaurant List
+ *     - Restaurant Cards
+ *        - Image
+ *        - Name
+ *        - Rating
+ *        - Cuisine
+ * Footer
+ *  - Links
+ *  - Copyright
+ * 
+ */
 
-// Creating nested React elements
-const heading = React.createElement("h1", {
-    id: "title-1",
-    key: "first",
-    style: {color: "#626fe6"},
-}, "Hello, React!");
+const Title = () => (
+    <a href="/">
+        <img className="logo" alt="logo" src="https://i.ibb.co/nMD7kzB/Hurry-Curry.jpg" />
+    </a>   
+)
 
-console.log(heading);
+const Header = () => {
+    return (
+        <div className="header">
+           <Title />
+           <div className="nav-items">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Cart</li>
+            </ul>
+           </div>
+        </div>
+    )
+}
 
-const heading2 = React.createElement("h1", {
-    id: "title-2",
-    key: "second",
-    style: {color: "#e8630a"},
-}, "Bonjour, React!");
+const RestaurantCard = () => {
+    return (
+        <div className="card">
+            <img src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/bdcd233971b7c81bf77e1fa4471280eb" />
+            <h3>KFC</h3>
+            <h2>Burgers, American, Fast Food</h2>
+            <div>
+              <h2>4.2 stars</h2>
+              <h2>19 mins</h2>
+            </div>
+        </div>
+    )
+}
 
-const heading3 = React.createElement("h1", {
-    id: "title-3",
-    key: "third",
-    style: {color: "f8b400"},
-}, "Ciao, React!");
+const Body = () => (
+    <>
+       <RestaurantCard />
+    </>
+)
 
-const container = React.createElement("div", {
-    id: "container",
-    key: "container-one",
-}, [heading, heading2, heading3]);
+const Footer = () => (
+    <h3>Footer</h3>
+)
 
-console.log(container);
+const AppLayout = () => {
+    return (
+        <>
+            <Header />
+            <Body />
+            <Footer />
+        </>
+    )
+}
 
-// Creating the root using createRoot
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// Rendering the elements
-root.render(container);
+root.render(<AppLayout />);
